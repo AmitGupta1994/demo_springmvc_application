4fdf161f536ca3c17d2de8b3cb80b82dca1d35f2
Amit Gupta <iamitgupta1994@gmail.com>
2020-04-05 13:04:33 +0545

    [add][dev]: use of ApplicationConfig,MvcConfig and MyWebInitializer to remove the work of DemoApp-servlet and web.xml

--- src/main/java/config/MyWebInitializer.java
+++ src/main/java/config/MyWebInitializer.java
@@ -10,7 +10,7 @@ public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletI
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] {ApplicationConfig.class};
+        return new Class[] {MvcConfig.class};
     }
 
     @Override
